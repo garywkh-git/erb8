bbb5bc768168b3e5dc11f129dd4542e30dcf22d6
garywkh-git <garywkh@gmail.com>
2025-12-24 14:36:16 +0800

    Step 5 : listings & doctors model

--- config/settings.py
+++ config/settings.py
@@ -45,7 +45,10 @@ DJANGO_APPS = [
 
 ]
 
-APPLICATION_APPS = ['pages.apps.PagesConfig']
+APPLICATION_APPS = [
+                    'pages.apps.PagesConfig',
+                    'doctors.apps.DoctorsConfig',
+                    'listings.apps.ListingsConfig']
 
 INSTALLED_APPS = DJANGO_APPS + APPLICATION_APPS
 
@@ -84,8 +87,11 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'clinic',
+        'USER': 'postgres',
+        'PASSWORD': '1234',
+        'HOST': 'localhost'
     }
 }
 
